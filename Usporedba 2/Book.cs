48e4abbb17527df261a8bd1ae7225f8d1a9e4fc4
Miljac7 <152974534+Miljac7@users.noreply.github.com>
2024-12-11 09:24:42 +0100

    Add files via upload

---
+++ Usporedba 2/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usporedba_2
+{
+    internal class Book
+    {
+        public string Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Image: {Image}, Name: {Name}";
+        }
+
+    }
+
+}
---
+++ Usporedba 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Usporedba_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var prviXml = XDocument.Load("prvi.xml");
+            var drugiXml = XDocument.Load("drugi.xml");
+
+            // Parse books using the Book class
+            var prviBooks = ParseBooks(prviXml);
+            var drugiBooks = ParseBooks(drugiXml);
+
+            // Find differences
+            var onlyInPrvi = prviBooks.Except(drugiBooks);
+            var onlyInDrugi = drugiBooks.Except(prviBooks);
+
+            Console.WriteLine("Books only in prvi.xml:");
+            foreach (var book in onlyInPrvi)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("\nBooks only in drugi.xml:");
+            foreach (var book in onlyInDrugi)
+            {
+                Console.WriteLine(book);
+            }
+        }
+
+        static IEnumerable<Book> ParseBooks(XDocument doc)
+        {
+            return doc.Descendants("book").Select(book => new Book
+            {
+                Id = (string)book.Attribute("id"),
+                Image = (string)book.Attribute("image"),
+                Name = (string)book.Attribute("name")
+            });
+        }
+    }
+
+    }
+
